a8514d3b4fa3c12dca24b24eea76c87ab5cc922a
Vlad Tyutyunov <fildranger@gmail.com>
2020-04-07 17:54:57 +0300

    added form [second-branch]

--- apps/login-form/src/app/app.module.ts
+++ apps/login-form/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginFormModule } from './login-form/login-form.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, LoginFormModule],
   providers: [],
   bootstrap: [AppComponent]
 })
